9fc92b03dbd0e3282436b1e63352abbaf0f8a304
spiritLHLS <103393591+spiritLHLS@users.noreply.github.com>
2023-05-08 10:54:32 +0800

    Create 预测值_实际值_散点趋势图.R

---
+++ 预测值_实际值_散点趋势图.R
@@ -0,0 +1,14 @@
+# 读取数据
+data <- read.csv("pcc_cpu_test_multi_cells_line.csv", header = TRUE)
+
+# 计算最小二乘回归系数
+fit <- lm(data[, 5] ~ data[, 4])
+a <- round(coef(fit)[2], 4)
+b <- round(coef(fit)[1], 18)
+
+# 绘制散点图和回归线
+plot(data[, 4], data[, 5], xlab = "Predicted Values", ylab = "Actual Values", main = "Predicted vs. Actual Values", col = "blue")
+abline(fit, col = "red")
+
+# 添加趋势线图例
+legend("topleft", legend = paste("y=", a, "x", b), col = "red", lty = 1, bty = "n")
