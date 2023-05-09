771892ed09c1ed4069478d41051a999d340964d3
spiritLHLS <103393591+spiritLHLS@users.noreply.github.com>
2023-05-09 12:07:07 +0800

    Create 热力图.R

---
+++ 热力图.R
@@ -0,0 +1,19 @@
+pdf("n_heatmap.pdf", width = 10, height = 10)
+
+# 读取csv文件，将第一列设置为行名
+data <- read.csv("newdata_Tanimoto.csv", row.names = 1)
+
+# 将列名设置为行名
+colnames(data) <- rownames(data)
+
+# 将数据框转换为数值矩阵
+data_matrix <- as.matrix(data)
+
+# 定义自定义颜色
+my_colors <- colorRampPalette(c("#FFFFFF", "#F0E442", "#F08080", "#800000"))
+
+# 绘制热力图
+heatmap(data_matrix, scale = "column", margins = c(10, 10), 
+        col = my_colors(100))
+
+dev.off()
