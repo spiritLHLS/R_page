8f2d35c5a78fd9cb8cd260616f817ab0de98a7e2
spiritLHLS <103393591+spiritLHLS@users.noreply.github.com>
2023-05-10 16:29:20 +0800

    Create combined.py

---
+++ combined.py
@@ -0,0 +1,86 @@
+import os
+import re
+# pip install python-docx
+from docx import Document
+
+def extract_chapter_names(file_path):
+    all_latex = []
+    with open(file_path, 'r') as file:
+        lines = file.readlines()
+        is_comment = False
+        for line in lines:
+            line = line.strip()
+            if line.startswith('%%') or line.startswith('%'):
+                continue
+            else:
+                try:
+                    chapter_name = line.split('{')[1].split('}')[0]
+                    if "chapter/" in chapter_name:
+                        all_latex.append(chapter_name.replace("chapter/",""))
+                except:
+                    pass
+    return all_latex
+
+file_path = 'template.tex'  # 替换为主文件名字
+chapter_names = extract_chapter_names(file_path)
+# print(chapter_names)
+
+def check_files_existence(chapter_names):
+    real_files = []
+    for name in chapter_names:
+        file_name = name + '.tex'
+        if os.path.isfile(file_name):
+            real_files.append(file_name)
+    return real_files
+real_files = check_files_existence(chapter_names)
+print(real_files)
+
+def filter_latex(file_path):
+    with open(file_path, 'r') as file:
+        content = file.read()
+    
+    pattern = r'\\label{chap.*?}'
+    content = re.sub(pattern, '', content, flags=re.DOTALL)
+        
+    pattern = r'\\begin{figure}(.*?)\\end{figure}'
+    content = re.sub(pattern, '', content, flags=re.DOTALL)
+    
+    pattern = r'\\begin{equation}(.*?)\\end{equation}'
+    content = re.sub(pattern, '', content, flags=re.DOTALL)
+    
+    pattern = r'\\begin{table}(.*?)\\end{table}'
+    content = re.sub(pattern, '', content, flags=re.DOTALL)
+    
+    content = re.sub(r'\\cite\{[^\}]*\}', '', content)
+    
+    content = re.sub(r'~\\ref\{[^\}]*\}', '', content)
+    
+    content = re.sub(r'\\ref\{[^\}]*\}', '', content)
+    
+    content = re.sub(r'^(?:\\begin|\\end|\\label\\includegraphics).*?\n', '', content, flags=re.MULTILINE)
+    
+    content = re.sub(r'%.*?\n', '', content)
+
+    content = re.sub(r'\\[a-zA-Z]+\*?', '', content)
+
+    content = re.sub(r'[{}]', '', content)
+
+    content = re.sub(r'\\[^{}\s]+(?:{[^{}]*})*', '', content)
+    
+    content = re.sub(r'\n\s*\n', '\n', content)
+    
+    return content.strip()
+
+files = []
+for file_path in real_files:
+    filtered_text = filter_latex(file_path)
+    files.append(filtered_text)
+
+def combine_text_to_word(files, output_filename):
+    doc = Document()
+    for text in files:
+        doc.add_paragraph(text)
+    doc.save(output_filename)
+
+output_filename = "combined.docx"
+combine_text_to_word(files, output_filename)
