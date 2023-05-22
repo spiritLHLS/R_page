07e42339da75e506b5da6afe1730893565083994
spiritlhl <103393591+spiritLHLS@users.noreply.github.com>
2023-05-22 18:08:14 +0800

    Update 三维散点图.R

--- 三维散点图.R
+++ 三维散点图.R
@@ -9,8 +9,6 @@ library(plot3D)
 #保存为pdf
 pdf("scatter3D.pdf", width = 10, height = 10)
 
-par(mar = c(5, 6, 4, 2) + 0.1)  # 调整左边的边距 底 左 顶 右
-
 # 读取数据
 data <- read_excel("583drugs39cellsynergy.xlsx", sheet = 1)
 # data <- read.csv("583drugs39cellsynergy.csv", header=TRUE, row.names=1)
@@ -27,6 +25,5 @@ z <- as.vector(as.matrix(data))
 # 绘制三维散点图
 scatter3D(x=x, y=y, z=z, colvar=z, bty="g", pch=16, cex=1.5, 
           theta=30, phi=10, xlab="Drugs Combination", ylab="Cells", zlab="Value", cex.lab = 2, cex.axis = 1.6)
-# cex.lab 坐标轴名字大小
-# cex.axis 坐标轴上字符大小
+          
 dev.off()
